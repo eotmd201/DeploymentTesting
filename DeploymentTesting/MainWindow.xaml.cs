3ca868e78f3f0aa0b9557b8ed0cdd0d51f6b4f01
조대승 <eotmd201@outlook.kr>
2024-09-04 16:15:07 +0900

    초기 커밋.

---
+++ DeploymentTesting/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeploymentTesting
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void label_Loaded(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt|All files (*.*)|*.*";
+            string currentDirectory = Directory.GetCurrentDirectory();
+            MessageBox.Show($"Current Directory: {currentDirectory}", "Current Directory");
+            File.WriteAllText("test.txt", "TEST Code");
+        }
+    }
+}
